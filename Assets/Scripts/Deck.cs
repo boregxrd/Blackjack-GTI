3d6661a01ef07ce1a67f253f3848028b7040e747
Boregxrd <manuelborregales12@gmail.com>
2024-05-05 15:52:16 +0200

    Modularized Probabilities Function

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -97,7 +97,7 @@ public class Deck : MonoBehaviour
         {
             finalMessage.text = "Has hecho Blackjack";
 
-            disableButtons();
+            DisableButtons();
 
             dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().ToggleFace(true);
         }
@@ -107,178 +107,176 @@ public class Deck : MonoBehaviour
             finalMessage.text = "El Dealer ha hecho Blackjack";
             dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().ToggleFace(true);
 
-            disableButtons();
+            DisableButtons();
 
             dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().ToggleFace(true);
         }
     }
 
+    private string CalculateGreater21Probability(int pointsToLose, GameObject player, GameObject dealer, int cardIndex)
+    {
+        float cardsThatSurpass = 12;
+
+        for (int i = pointsToLose; i <= 9; i++)
+        {
+            cardsThatSurpass += 4;
+        }
+
+        for (int i = 0; i < dealer.GetComponent<CardHand>().cards.Count; i++)
+        {
+            if (dealer.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= pointsToLose && i > 0)
+            {
+                cardsThatSurpass -= 1;
+            }
+        }
+
+        for (int i = 0; i < player.GetComponent<CardHand>().cards.Count; i++)
+        {
+            if (player.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= pointsToLose)
+            {
+                cardsThatSurpass -= 1;
+            }
+        }
+
+        return (cardsThatSurpass / (52.0 - cardIndex * 0.0)).ToString("F2");
+    }
+
+    private string CalculateBetween17Probability(int cardsTill17, int cardsTill21, GameObject player, GameObject dealer, int cardIndex)
+    {
+        float cardsThatGoIn = 0;
+
+        for (int i = cardsTill17; i <= cardsTill21; i++)
+        {
+            if (i == 10)
+            {
+                cardsThatGoIn += 12;
+            }
+            else
+            {
+                cardsThatGoIn += 4;
+            }
+        }
+
+        for (int i = 0; i < dealer.GetComponent<CardHand>().cards.Count; i++)
+        {
+            if (dealer.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= cardsTill17 && dealer.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value <= cardsTill21 && i > 0)
+            {
+                cardsThatGoIn -= 1;
+            }
+        }
+
+        for (int i = 0; i < player.GetComponent<CardHand>().cards.Count; i++)
+        {
+            if (player.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= cardsTill17 && player.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value <= cardsTill21)
+            {
+                cardsThatGoIn -= 1;
+            }
+        }
+
+        return (cardsThatGoIn / (52.0 - cardIndex * 0.0)).ToString("F2");
+    }
+
+    private string CalculateHiddenCardProbability(int playerPoints, int dealerPoints, int diffPoints, GameObject player, GameObject dealer, int cardIndex)
+    {
+        float cardsThatGoIn = 0;
+
+        if (diffPoints > 11)
+        {
+            return "0.0";
+        }
+
+        for (int i = diffPoints++; i <= 11; i++)
+        {
+            if (i == 10)
+            {
+                cardsThatGoIn += 12;
+            }
+            else
+            {
+                cardsThatGoIn += 4;
+            }
+        }
+
+        for (int i = 0; i < dealer.GetComponent<CardHand>().cards.Count; i++)
+        {
+            if (dealer.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= diffPoints && i > 0)
+            {
+                cardsThatGoIn -= 1;
+            }
+        }
+
+        for (int i = 0; i < player.GetComponent<CardHand>().cards.Count; i++)
+        {
+            if (player.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= diffPoints)
+            {
+                cardsThatGoIn -= 1;
+            }
+        }
+
+        return (cardsThatGoIn / (52.0 - cardIndex * 0.0)).ToString("F2");
+    }
+
     private void CalculateProbabilities()
     {
-
         // Probabilidad de que el jugador obtenga más de 21 si pide una carta
         string greater21Prob = "";
+        string between17Prob = "";
+        string hiddenCardProb = "";
 
+        int pointsToLose = 22 - player.GetComponent<CardHand>().points;
+        int cardsTill17 = 0;
+        int cardsTill21 = 21 - player.GetComponent<CardHand>().points;
+        int playerPoints = player.GetComponent<CardHand>().points;
+        int dealerPoints = dealer.GetComponent<CardHand>().points - dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().value;
+        int diffPoints = playerPoints - dealerPoints;
+
+        // Cálculo de la probabilidad para obtener más de 21
         if (player.GetComponent<CardHand>().points < 12)
         {
             greater21Prob = "\n X > 21: 0.0";
         }
         else
         {
-            int pointsToLose = 22 - player.GetComponent<CardHand>().points;
-            float cardsThatSurpass = 12;
-
-            for (int i = pointsToLose; i <= 9; i++)
-            {
-                cardsThatSurpass += 4;
-            }
-
-            for (int i = 0; i < dealer.GetComponent<CardHand>().cards.Count; i++)
-            {
-                if (dealer.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= pointsToLose && i > 0)
-                {
-                    cardsThatSurpass -= 1;
-                }
-            }
-
-            for (int i = 0; i < player.GetComponent<CardHand>().cards.Count; i++)
-            {
-                if (player.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= pointsToLose)
-                {
-                    cardsThatSurpass -= 1;
-                }
-            }
-
-            greater21Prob = "\n X > 21: " + (cardsThatSurpass / (52.0 - cardIndex * 0.0)).ToString("F2");
+            greater21Prob = "\n X > 21: " + CalculateGreater21Probability(pointsToLose, player, dealer, cardIndex);
         }
 
-        string between17Prob = "";
-
-        int cardsTill17 = 0;
-        int cardsTill21 = 21 - player.GetComponent<CardHand>().points;
-        float cardsThatGoIn = 0;
-
-        if (player.GetComponent<CardHand>().points < 17)
-        {
-            cardsTill17 = 17 - player.GetComponent<CardHand>().points;
-        }
-
-        if (cardsTill21 > 11)
-        {
-            cardsTill21 = 11;
-        }
-
+        // Cálculo de la probabilidad para obtener entre 17 y 21
         if (player.GetComponent<CardHand>().points < 6)
         {
             between17Prob = "\n 17<=X<=21: 0.0";
         }
         else
         {
-
-            for (int i = cardsTill17; i <= cardsTill21; i++)
-            {
-                if (i == 10)
-                {
-                    cardsThatGoIn += 12;
-                }
-                else
-                {
-                    cardsThatGoIn += 4;
-                }
-
-            }
-
-            for (int i = 0; i < dealer.GetComponent<CardHand>().cards.Count; i++)
-            {
-                if (dealer.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= cardsTill17 && dealer.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value <= cardsTill21 && i > 0)
-                {
-                    cardsThatGoIn -= 1;
-                }
-            }
-
-            for (int i = 0; i < player.GetComponent<CardHand>().cards.Count; i++)
-            {
-                if (player.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= cardsTill17 && player.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value <= cardsTill21)
-                {
-                    cardsThatGoIn -= 1;
-                }
-            }
-
-            between17Prob = "\n 17<=X<=21: " + (cardsThatGoIn / (52.0 - cardIndex * 0.0)).ToString("F2");
+            cardsTill17 = player.GetComponent<CardHand>().points < 17 ? 17 - player.GetComponent<CardHand>().points : 0;
+            cardsTill21 = cardsTill21 > 11 ? 11 : cardsTill21;
+            between17Prob = "\n 17<=X<=21: " + CalculateBetween17Probability(cardsTill17, cardsTill21, player, dealer, cardIndex);
         }
 
-        string hiddenCardProb = "";
-        int playerPoints = player.GetComponent<CardHand>().points;
-        int dealerPoints = dealer.GetComponent<CardHand>().points - dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().value;
-        int diffPoints = 0;
-
+        // Cálculo de la probabilidad de que la carta oculta del dealer supere la del jugador
         if (playerPoints < dealerPoints)
         {
             hiddenCardProb = "\n Deal > Play:  1.0";
         }
         else
         {
-
-            diffPoints = playerPoints - dealerPoints;
-
-            if (diffPoints > 11)
-            {
-                hiddenCardProb = "\n Deal > Play:  0.0";
-            }
-            else
-            {
-                cardsThatGoIn = 0;
-
-                for (int i = diffPoints++; i <= 11; i++)
-                {
-
-                    if (i == 10)
-                    {
-                        cardsThatGoIn += 12;
-                    }
-                    else
-                    {
-                        cardsThatGoIn += 4;
-                    }
-
-                }
-
-                for (int i = 0; i < dealer.GetComponent<CardHand>().cards.Count; i++)
-                {
-                    if (dealer.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= diffPoints && i > 0)
-                    {
-                        cardsThatGoIn -= 1;
-                    }
-                }
-
-                for (int i = 0; i < player.GetComponent<CardHand>().cards.Count; i++)
-                {
-                    if (player.GetComponent<CardHand>().cards[i].GetComponent<CardModel>().value >= diffPoints)
-                    {
-                        cardsThatGoIn -= 1;
-                    }
-                }
-
-                hiddenCardProb = "\n Deal > Play:  " + (cardsThatGoIn / (52.0 - cardIndex * 0.0)).ToString("F2");
-            }
-
-            probMessage.text = hiddenCardProb + "\n\n" + between17Prob + "\n\n" + greater21Prob;
+            hiddenCardProb = "\n Deal > Play:  " + CalculateHiddenCardProbability(playerPoints, dealerPoints, diffPoints, player, dealer, cardIndex);
         }
 
+        probMessage.text = hiddenCardProb + "\n\n" + between17Prob + "\n\n" + greater21Prob;
     }
 
     void PushDealer()
     {
         dealer.GetComponent<CardHand>().Push(faces[cardIndex], values[cardIndex]);
         cardIndex++;
-        updatePoints();
+        UpdatePoints();
     }
 
     void PushPlayer()
     {
         player.GetComponent<CardHand>().Push(faces[cardIndex], values[cardIndex]/*,cardCopy*/);
         cardIndex++;
-        updatePoints();
+        UpdatePoints();
         CalculateProbabilities();
     }
 
@@ -292,13 +290,13 @@ public class Deck : MonoBehaviour
         {
             finalMessage.text = "Oh... ¡Te has pasado!";
 
-            disableButtons();
+            DisableButtons();
         }
         else if(player.GetComponent<CardHand>().points == 21) 
         {
             finalMessage.text = "Has hecho Blackjack";
 
-            disableButtons();
+            DisableButtons();
         }
     }
 
@@ -319,7 +317,7 @@ public class Deck : MonoBehaviour
                 if (player.GetComponent<CardHand>().points < dealer.GetComponent<CardHand>().points && dealer.GetComponent<CardHand>().points <= 21)
                 {
                     dealerWinsWOBlackJack = true;
-                    updatePoints();
+                    UpdatePoints();
                     finalMessage.text = "¡Has perdido!";
                 }
                 else if (player.GetComponent<CardHand>().points == dealer.GetComponent<CardHand>().points)
@@ -335,14 +333,14 @@ public class Deck : MonoBehaviour
 
                 end = true;
 
-                disableButtons();
+                DisableButtons();
             }
         } 
     }
 
     public void PlayAgain()
     {
-        enableButtons();
+        EnableButtons();
         finalMessage.text = "";
         bankMessage.text = "Banca: " + player.GetComponent<CardHand>().bank.ToString() + " €";
         betMessage.text = "Apuesta: 0 €";
@@ -353,7 +351,7 @@ public class Deck : MonoBehaviour
         StartGame();
     }
 
-    public void apostar()
+    public void Bet()
     {
         if (player.GetComponent<CardHand>().bank != 0)
         {
@@ -364,21 +362,21 @@ public class Deck : MonoBehaviour
         }
     }
 
-    private void disableButtons()
+    private void DisableButtons()
     {
         hitButton.interactable = false;
         stickButton.interactable = false;
         apostarButton.interactable = false;
     }
 
-    private void enableButtons()
+    private void EnableButtons()
     {
         hitButton.interactable = true;
         stickButton.interactable = true;
         apostarButton.interactable = true;
     }
 
-    private void updatePoints()
+    private void UpdatePoints()
     {
         playerPoints.text = "Puntos: " + player.GetComponent<CardHand>().points.ToString();
         dealerPoints.text = "Puntos: " + dealer.GetComponent<CardHand>().dealerSecondCard;
